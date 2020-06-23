072ac2de69928c9b3b45c4e1a7bb16a13e38d221
angelort <64110564+angelort@users.noreply.github.com>
2020-06-23 18:05:24 -0500

    Add files via upload

---
+++ Challengue_20/Challenge#21/script.js
@@ -0,0 +1,11 @@
+let contador;
+for (contador = 1; contador <= 10; contador++) {
+  console.log('Vuelta' + " " + contador);
+};
+
+let vuelta;
+vuelta=1;
+while (vuelta<=10){
+    console.log('Vuelta número' + " " + vuelta);
+    vuelta++;
+    };
