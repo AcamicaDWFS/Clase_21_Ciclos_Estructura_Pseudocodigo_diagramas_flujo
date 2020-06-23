c8534aeabfa08f7c3e242e9dadd6f48b8e4daf8d
celara91 <62780873+celara91@users.noreply.github.com>
2020-06-22 23:14:29 -0500

    Add files via upload

---
+++ Challengue_20/ch_11_for.js
@@ -0,0 +1,6 @@
+var trian = "";
+
+for (var i = 0; i < 10; ++i ) {
+    trian = trian + "*";
+    console.log(trian);
+}
---
+++ Challengue_20/ch_11_while.js
@@ -0,0 +1,7 @@
+var rep = 0;
+
+console.log(rep <= 10);
+while (rep <= 10) {
+   console.log(rep);
+    ++rep;
+}
