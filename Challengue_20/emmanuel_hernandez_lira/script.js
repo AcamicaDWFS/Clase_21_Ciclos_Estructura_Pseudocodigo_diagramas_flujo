696cfb3491e15689739c83c9be886938a47c1bc2
Emmanuel Hernandez <calcetin.izquierdo@gmail.com>
2020-06-23 16:55:55 -0500

    Challenge completed.

---
+++ Challengue_20/emmanuel_hernandez_lira/script.js
@@ -0,0 +1,15 @@
+// Primer ciclo.
+
+console.log("Ciclo 1:");
+for (let i = 0; i < 10; i++) {
+  console.log(i);
+}
+
+// Segundo ciclo.
+
+console.log("Ciclo 2:");
+let i = 0;
+while (i < 10) {
+  console.log(i);
+  i++;
+}
