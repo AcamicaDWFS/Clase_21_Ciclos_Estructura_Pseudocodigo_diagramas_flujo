bde53e0d26fb58fca6b82df6206147ed433b7281
silvanahernandez <tanis_1891@hotmail.com>
2020-06-23 18:31:29 -0500

    Challenge terminado

---
+++ Challengue_20/Challenge_Bitacora21_Tanya_Hernandez_Valdez/script.js
@@ -0,0 +1,13 @@
+var ciclo;
+for (ciclo = 1; ciclo <= 10; ciclo++) {
+  // Se ejecuta 5 veces, con valores desde paso desde 0 hasta 4.
+  console.log('Ciclo for' + " " + ciclo);
+};
+
+n = 0;
+x = 0;
+while (n < 10) {
+  n++;
+  x += n;
+  console.log('Ciclo while' + " " + n);
+}
